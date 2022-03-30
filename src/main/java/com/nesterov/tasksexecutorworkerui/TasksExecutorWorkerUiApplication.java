0bd855947259a3cf288f8f0a6a477aeb2991d432
BabyBurrito <mywketep00@mail.ru>
2022-03-30 16:04:00 +0300

    init

---
+++ src/main/java/com/nesterov/tasksexecutorworkerui/TasksExecutorWorkerUiApplication.java
@@ -0,0 +1,13 @@
+package com.nesterov.tasksexecutorworkerui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksExecutorWorkerUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksExecutorWorkerUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nesterov/tasksexecutorworkerui/TasksExecutorWorkerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesterov.tasksexecutorworkerui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksExecutorWorkerUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
